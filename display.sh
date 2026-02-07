c96761abab5bf2cdf94cc02f5db21d6756028562
lumen-novum <novablackxd@gmail.com>
2026-02-07 15:46:19 -0600

    New portfolio

--- display.sh
+++
@@ -1,4 +0,0 @@
-clear
-echo "caysen-mayo@UTD ~ % neofetch"
-neofetch --config neofetch.conf --source UTD.txt
-read -p "caysen-mayo@UTD ~ % " # Pause the program to show blinking cursor
--- mp4-to-gif.sh
+++
@@ -1,2 +0,0 @@
-# More of an alias than anything - just so I don't forget how to do it :)
-ffmpeg -i $1.mp4 $1.gif
